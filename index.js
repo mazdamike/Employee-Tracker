a4af59d1a15643fdebfa5c02c7ebbaf4a758c3a4
mazdamike <mwpowell44@gmail.com>
2020-05-17 18:08:55 -0400

    added packages and created connection

---
+++ index.js
@@ -0,0 +1,29 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "root",
+  database: "employee_trackerdb"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  console.log("****************" + "\n" + "Employee Tracker" + "\n" + "****************");
+  startEmployeeTracker();
+});
+
+function startEmployeeTracker() {
+  console.log("start program");    
+}
